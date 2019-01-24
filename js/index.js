0d52587a2294c168389070b5f4fb1a0d558fee32
minusdan <36491025+minusdan@users.noreply.github.com>
2019-01-24 13:51:43 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var userFeed = new Instafeed({
+  get: 'user',
+  userId: '8987997106',
+  clientId: '924f677fa3854436947ab4372ffa688d',
+
+  resolution: 'standard_resolution',
+  template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
+  sortBy: 'most-recent',
+  limit: 12,
+  links: false
+});
+userFeed.run();
+/*  accessToken: '8987997106.924f677.8555ecbd52584f41b9b22ec1a16dafb9',  */
